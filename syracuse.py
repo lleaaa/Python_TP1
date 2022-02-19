8cd15edad596c135cbc6b6894be8bbdcc030bdad
Léa Jesson <jesson@et.esiea.fr>
2022-02-19 19:14:12 +0100

    third

--- syracuse.py
+++ syracuse.py
@@ -1,12 +1,14 @@
-def syracuse(N,n):
-    u = N
-    
-    for i in range(1, n+1): 
-        if u%2==0: 
-            u = u//2 
+def syracuse(n):
+    i=0
+    while n!=1:
+        print(n) 
+        if n%2==0: 
+            n = n//2 
         else:
-            u = 3*u+1
-        print(u)
+            n = 3*n+1
+        
+        i+=1
+    print("Le 1 a été trouvé apres", i,"iterations!")
    
 
-syracuse(15,5)
+syracuse(15)
